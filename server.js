6a94e8cb2675dd6b8a89af58f0fc6372efcb2699
Joel Ansbro <joelmatthewansbro@gmail.com>
2021-02-22 01:00:59 +0000

    css styling
    Created some more css styling, modified the navbar and sorted links into some div containers in order to make the application look more similar to Amazon.

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.use( express.static( "public" ) );
 
 app.get('/', async (req, res)=>{
     const { domain } = req.query;
