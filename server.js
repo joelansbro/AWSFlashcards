ed490a5a6580f332292aeb9021c69ebb00a662aa
Joel Ansbro <joelmatthewansbro@gmail.com>
2021-03-07 23:44:50 +0000

    added ejs-mate
    Added ejs-mate to handle templating via a boilerplate for easier editing. This change has meant that the bottom bar has ceased to show on the pages. I will have to edit the styles in the next major change.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const awsBase = require('./models/users');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/aws', {useNewUrlParser: true, useUnifiedTopology: true})
@@ -14,8 +15,10 @@ mongoose.connect('mongodb://localhost:27017/aws', {useNewUrlParser: true, useUni
         console.log(err);
     })
 
-app.set('views', path.join(__dirname, 'views'));
+app.engine("ejs", ejsMate);
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use( express.static( "public" ) );
