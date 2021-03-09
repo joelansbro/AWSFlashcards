767819df3a06f9095ecf41ec596fe9efee706088
Joel Ansbro <joelmatthewansbro@gmail.com>
2021-03-09 23:12:39 +0000

    moved styles.css to static
    moved the css over to a separate file to clear up the header

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
-app.use( express.static( "public" ) );
+app.use( express.static("public" ) );
 
 
 // index page to show all files //
